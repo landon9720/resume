bb59f8f79b897b192d5c64d4d8599c9a59fae831
Landon Kuhn <landon9720@gmail.com>
2017-10-17 18:16:51 -0700

    bootstrapped HashRouter

--- frontend.jsx
+++ frontend.jsx
@@ -1,43 +1,64 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import data from './data.json'
+import { Route, HashRouter } from 'react-router-dom'
 
-class Root extends React.Component {
-  render () {
-    return (
-      <div>
-        <h1 dangerouslySetInnerHTML={{__html: data.name}} />
+const App = () => (
+  <div>
+      <Route exact path="/" component={Resume} />
+      <Route path="/:id" component={Page} />
+  </div>
+)
+
+const Resume = () => (
+    <div>
+        <h1 dangerouslySetInnerHTML={{ __html: data.name }} />
         <p>
-          <span dangerouslySetInnerHTML={{__html: data.city}} /><br/>
-          <span dangerouslySetInnerHTML={{__html: data.phone}} /><br/>
-          <span dangerouslySetInnerHTML={{__html: data.email}} /><br/>
-          <span dangerouslySetInnerHTML={{__html: data.resumeLink}} /><br/>
+            <span dangerouslySetInnerHTML={{ __html: data.city }} />
+            <br />
+            <span dangerouslySetInnerHTML={{ __html: data.phone }} />
+            <br />
+            <span dangerouslySetInnerHTML={{ __html: data.email }} />
+            <br />
+            <span dangerouslySetInnerHTML={{ __html: data.resumeLink }} />
+            <br />
         </p>
-        
-        <p dangerouslySetInnerHTML={{__html: data.status}}/>
-        
+
+        <p dangerouslySetInnerHTML={{ __html: data.status }} />
+
         <h2>Summary</h2>
-        {data.summaryStatements.map((s, i) => <p key={i} dangerouslySetInnerHTML={{__html: s}} />)}
-        
+        {data.summaryStatements.map((s, i) => <p key={i} dangerouslySetInnerHTML={{ __html: s }} />)}
+
         <h2>Experience</h2>
-        {data.experience.map((e, i) => <div key={i}>
-          <h4>{e.name}</h4>
-          <em>{e.title} - {e.location} - {e.time}</em>
-          <blockquote dangerouslySetInnerHTML={{__html: e.companySummary}} />
-          {e.narrative.map((n, i) => <p key={i} dangerouslySetInnerHTML={{__html: n}} />)}
-        </div>)}
+        {data.experience.map((e, i) => (
+            <div key={i}>
+                <h4>{e.name}</h4>
+                <em>
+                    {e.title} - {e.location} - {e.time}
+                </em>
+                <blockquote dangerouslySetInnerHTML={{ __html: e.companySummary }} />
+                {e.narrative.map((n, i) => <p key={i} dangerouslySetInnerHTML={{ __html: n }} />)}
+            </div>
+        ))}
 
         <h2>Education</h2>
-        <em><span dangerouslySetInnerHTML={{__html: data.education.where}}/> - {data.education.what} - {data.education.when}</em>
-        <p dangerouslySetInnerHTML={{ __html: data.education.statement}} />
-      </div>
-    )
-  }
-}
+        <em>
+            <span dangerouslySetInnerHTML={{ __html: data.education.where }} /> - {data.education.what}  -{' '}
+            {data.education.when}
+        </em>
+        <p dangerouslySetInnerHTML={{ __html: data.education.statement }} />
+    </div>
+)
 
-
+const Page = props => (
+  <div>
+    Hi {props.match.params.id}
+  </div>
+)
 
 ReactDOM.render(
-  <Root />,
-  document.getElementById('root')
+    <HashRouter hashType="noslash">
+        <App />
+    </HashRouter>,
+    document.getElementById('root')
 )
