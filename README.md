b31b0ed147c482ae17d7c3206bd01ff7ccd85cb1
Landon Kuhn <landon9720@gmail.com>
2018-03-06 07:45:07 -0800

    updates

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Landon Kuhn
 
-Please see my resume at [landon9720.com](http://landon9720.com).
+This is the source repository for my personal website and resume. Please see the finished product at [http://landon9720.com](http://landon9720.com).
+
+# Build and deploy
+
+```
+# run locally at http://localhost:8080
+yarn serv
+
+# build
+yarn build
+
+# deploy
+yarn deploy
+```
--- pages/resume.jsx
+++ pages/resume.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default () => (
-    <div className='resume'>
+    <div className="resume">
         <h1>
             <Link to="/helloworld">Landon Kuhn</Link>
         </h1>
@@ -15,72 +15,76 @@ export default () => (
             <Link to="/landon9720">landon9720.com</Link>
             <br />
         </p>
-        <p>
-            I am currently Software Architect at <a href="http://sunsama.com">Sunsama</a>.
-        </p>
-
         <h2>Summary</h2>
         <p>
-            I am a full-stack software engineer experienced in web and mobile application development, web APIs and
-            services, data processing, storage, and analytics. I work at all stages of software development:
-            requirements, architecture, implementation, test, deployment, operation, monitoring, and support.
+            I invent, build, and operate web software. I am a prolific full-time coder. I focus on the right problem at
+            the right time, enabled by experience and judgement. I find and highlight the direct path. I unambiguate and
+            deliver.
         </p>
         <p>
-            I develop and operate production software in languages C, C++, C#, Java, JavaScript, Objective-C, and Scala.
-            I write for environments including web browsers, Linux, AWS, iOS, and PC.
+            I work in the domain of web software development and operation: requirements, architecture, implementation,
+            test, deployment, operation, monitoring, scaling, and support.
+        </p>
+        <p>I have professional experience in programing languages including:</p>
+        <ul>
+            <li>JavaScript</li>
+            <li>Java, Scala</li>
+            <li>Objective-C</li>
+            <li>Perl, Python, Bash</li>
+            <li>C, C++</li>
+            <li>C#</li>
+        </ul>
+        <p>
+            I write for runtime environments including Amazon Web Services (AWS), web browsers, mobile devices, and PC.
         </p>
         <p>
-            I have experience with many web technologies and frameworks including AJAX, ASP.NET, Bootstrap, ECMAScript
-            2015, Guava, HTML, HTTP, JSON, JSP, MVC, PHP, Play, REST, React, SOAP, Servlet, Spray.io, Spring, Tomcat,
-            Underscore, XML, jQuery.
+            I practice iterative development, unit testing, continuous integration, and automated deployment. I prefer
+            functional programming. Tools I use include Visual Studio, Visual Studio Code, Intellij IDEA, Eclipse,
+            ClearCase, Git, Perforce, Subversion, JIRA, Trello, Jenkins, Maven, SBT, Bash, Google Chrome, and Slack.
         </p>
-        <p>
-            I store data using MySQL, MSSQL, PostgreSQL, DynamoDB, Redshift, S3, Lucene, and Neo4j. I access and
-            transform data using SQL, Apache Spark, Apache Ignite, Amazon EMR, ETL, and MDX. I build data visualizations
-            using D3, Google Charts, Google Maps, and Tableau.
-        </p>
-        <p>
-            I have experience operating software in colocation centers, on-premises, and in Amazon Web Services. I use
-            AWS products including EC2, Elastic Beanstalk, RDS, CloudWatch, SNS, SQS, S3, DynamoDB, Redshift, Kinesis,
-            and EMR.
-        </p>
-        <p>I have led teams of up to 8 individual contributors.</p>
-        <p>
-            I practice iterative development, continuous integration, and automated deployment. Other tools I use
-            include Visual Studio, Visual Studio Code, Intellij IDEA, Eclipse, ClearCase, Git, Perforce, Subversion,
-            JIRA, Trello, Maven, SBT, and Bash.
-        </p>
-        <p>
-            My extracurricular programming interests include simulations, genetic algorithms, and computer-generated
-            music.
-        </p>
-
+        <p>My programming hobbies include simulations, genetic algorithms, and computer-generated music.</p>
         <h2>Experience</h2>
-
+        <h3>Sunsama</h3>
+        <em>Software Architect - Portland, Oregon - 2017-2018</em>
+        <blockquote>
+            <a href="https://sunsama.com">Sunsama</a> is a team calendar and task management application.
+        </blockquote>
+        <p>At Sunsama I work the entire development and operational stack, including:</p>
+        <ol>
+            <li>Component and container-level JSX (React)</li>
+            <li>Front-end API (NodeJS)</li>
+            <li>Data transport for real-time multi-user rich-text editor (Socket.io)</li>
+            <li>Native desktop application (Electron)</li>
+            <li>Build and deployment automation (Jenkins, Docker, Bash, EC2 Container Service)</li>
+            <li>Operational monitoring and emergency support</li>
+            <li>Software development process management (scrum)</li>
+        </ol>
         <h3>MobileRQ</h3>
         <em>Software Architect - Portland, Oregon - 2015-2017</em>
         <blockquote>
-            <a href="http://mobilerq.com">MobileRQ</a> is a platform that enables targeted content delivery and push
-            notifications for mobile and web applications. MobileRQ was acquired by{' '}
-            <a href="http://kochava.com">Kochava</a> in 2017.
+            <a href="http://mobilerq.com">MobileRQ</a> enables targeted content delivery and push notifications for
+            mobile and web applications. MobileRQ was acquired by <a href="http://kochava.com">Kochava</a> in 2017.
         </blockquote>
         <p>
             I developed, operated, and supported MobileRQ's software products, including web applications and services
-            on AWS, and SDKs for iOS and Android. My responsibilities included all aspects of the software stack,
-            including front-end (jQuery, Bootstrap), web tier (Spring MVC/Jersey), data stores (RDS/DynamoDB/S3), and
-            ETL (SQL/Spark). I worked with Scala, Java, and Amazon Web Services daily. I implemented major features and
-            improvements including:
+            on AWS, and iOS SDK. My responsibilities included all aspects of the software stack:
         </p>
         <ol>
-            <li>
-                A distributed matching engine built on Apache Ignite that evaluates customer-defined predicates with
-                customer data. Customer data features are defined using composed hierarchies of monadic transformations.
-                Customer predicates are evaluated in real-time using distributed data flow and compute architecture.
-                This implementation allows MobileRQ to meet a diversity of customer businesses and use-cases.
-            </li>
-            <li>Architectural refractors, resulting in order-of-magnitude cost saving and reliability improvement.</li>
+            <li>User interface (HTML, CSS, jQuery, Bootstrap)</li>
+            <li>Front-end API (Java, AWS Elastic Beanstalk)</li>
+            <li>Data storage and transformation (Scala, Apache Spark, RDS/PostgreSQL, DynamoDB, S3)</li>
         </ol>
-
+        <p>I implemented major features and improvements including:</p>
+        <ol>
+            <li>
+                A distributed matching engine that evaluates customer-defined predicates with customer data (Scala,
+                Apache Ignite)
+            </li>
+            <li>
+                Architectural refractors, resulting in order-of-magnitude cost saving and reliability improvement
+                (Scala, AWS Kinesis, AWS DynamoDB)
+            </li>
+        </ol>
         <h3>Janrain</h3>
         <em>Software Architect - Portland, Oregon - 2013-2015</em>
         <blockquote>
@@ -88,15 +92,15 @@ export default () => (
             web sites and mobile applications.
         </blockquote>
         <p>
-            Janrain products are implemented as services configured for individual customer requirements. At Janrain, I
-            led the effort to unify configuration data in a single system of record. My contributions included design
-            and implementation of services for managing configuration data, featuring a DSL for modeling and
-            transforming data over a graph database with compile-time correctness guarantees.
+            At Janrain I unified service configuration into a single system of record. I contributed design and
+            implementation of services for managing configuration data, featuring a DSL for modeling and transforming
+            data over a graph database with compile-time correctness guarantees. (Scala)
         </p>
         <p>
             Janrain services produce profile and event data from sources including social networks, web forms, and user
-            behavior. At Janrain, I led the effort to build analytics products that leverage this data. My
-            responsibilities included project architecture, vendor evaluations, and project planning and leadership.
+            action. At Janrain, I built analytics products that leverage this data. My responsibilities included
+            architecture, vendor evaluations, and project planning and leadership. I implemented ETL and data access
+            (Scala, Apache Spark, AWS EMR, S3, AWS Redshift).
         </p>
         <p>
             Janrain manages engineering projects using the methodologies of scrum, continuous delivery, and iterative
@@ -104,7 +108,6 @@ export default () => (
             contributors, and drove stakeholder understanding and alignment. I facilitated process meetings, including
             sprint planning, sprint retrospective, and demonstrations of progress.
         </p>
-
         <h3>Marin Software</h3>
         <em>Senior Engineer - Portland, Oregon - 2012</em>
         <blockquote>
@@ -113,40 +116,33 @@ export default () => (
         </blockquote>
         <p>
             I developed search features and performance improvements. I developed a data pipeline for asynchronously
-            loading search and analytic data into the front-end web application.
+            loading search and aggregate data into the front-end web application. (Java, JavaScript)
         </p>
-
         <h3>ClearAccess Inc.</h3>
         <em>Senior Engineer - Vancouver, Washington - 2009-2012</em>
         <blockquote>
-            ClearAccess was acquired by Cisco Systems in March 2012. ClearAccess provided Auto-Configuration Server
-            (ACS) software and customer-presence equipment (CPE) firmware to deliver flexible and scalable device and
-            application management based on the Broadband Forum's TR-069 standard.
+            ClearAccess provided Auto-Configuration Server (ACS) software and customer-premises equipment (CPE) firmware
+            to deliver flexible and scalable device and application management based on the Broadband Forum's TR-069
+            standard. ClearAccess was acquired by Cisco Systems in March 2012.
         </blockquote>
-        <p>
-            I was responsible for development of the ACS product, including: 1. web services and background processes
-            implemented in Scala and Java, 2. user interfaces utilizing AJAX libraries and patterns, 3. data storage
-            (MySQL, PostgreSQL, and Oracle Database, Mongo, Solr) and ORM (Hibernate), and 4. device communication
-            scripting using server-side JavaScript interpreter.
-        </p>
-        <p>
-            I developed the search and reporting stack, including: 1. object-to-document mapping library converts
-            relational business entities to search documents, 2. background process services requests to index business
-            entities and other index maintenance tasks, 3. domain-specific language parser for representing complex
-            search queries, and 4. reusable UI components for integrating search and reporting features throughout the
-            product.
-        </p>
-        <p>
-            I was responsible for operational support of production issues, including troubleshooting production
-            environments, monitoring and diagnostics, and fixing coding errors.
-        </p>
-        <p>
-            I provided project management, including work prioritization. I communicated with stakeholders and produced
-            technical requirements, with focus on customer satisfaction and software architecture. I developed the
-            release process to produce deliverables including build artifacts and documentation.
-        </p>
-
-        <h3>Qualcomm, Inc.</h3>
+        <p>I developed web applications and services, including:</p>
+        <ol>
+            <li>
+                Web applications for management of wireless access points, cable and DSL modems, and NAT devices (Scala,
+                Java, Tomcat)
+            </li>
+            <li>
+                Implementation of TR-069, the protocol used to manage customer-premises equipment (Scala, Java, Tomcat,
+                HTTP, XML)
+            </li>
+            <li>Data storage and transformation (MySQL, PostgreSQL)</li>
+            <li>Device communication scripting (Java/JavaScript)</li>
+            <li>
+                Search and reporting features, including data indexing engine, custom query language, and user interface
+                (Scala, Solr)
+            </li>
+        </ol>
+        <h3>QUALCOMM, Inc.</h3>
         <em>Senior Engineer - Portland, Oregon - 2001-2009</em>
         <blockquote>
             <a href="http://qualcomm.com">Qualcomm</a> designs, manufactures, and markets wireless telecommunications
@@ -154,28 +150,17 @@ export default () => (
             services.
         </blockquote>
         <p>
-            I advanced adoption of hosted web services using J2EE, AJAX, and related technologies. I lead technology and
-            best practice adoption. I advanced the use of unit testing through advocacy, presentation, mentoring, and
-            championing related techniques such as dependency injection and mock objects.
+            I implemented application features used by human drivers of hong-haul tractor-trailers. I developed
+            applications for real-time monitoring of freight. I created algorithms for processing driver logs according
+            to FMCSA Hours-of-Service regulations. I worked with position data, map visualization, geofencing, and SMS
+            alerts. I maintained build scripts. These projects provided abundant opportunities for learning and growth.
+            (C, C++, C#, Windows CE, Windows CE.NET, ASP.NET, Java, Perl, Python)
         </p>
+        <p>I was an early promoter of unit testing, dependency injection/inversion of control, mock objects.</p>
         <p>
-            I created an engine for processing long-haul driver logs according to the FMCSA Hours-of-Service
-            regulations. I implemented the engine in C for maximum source compatibility across platforms, and provided
-            Java and .NET interop.
+            I supported the IT infrastructure for the Portland office, including workstations for 50+ employees,
+            on-premises servers, and other LAN, WAN, and telecommunications equipment.
         </p>
-        <p>I developed a HTTP service for generating PDF and HTML reports.</p>
-        <p>I developed a service for delivery of email and SMS alert messages based on application-defined logic.</p>
-        <p>
-            I developed a J2EE web application for real-time monitoring of valuable freight, included map visualization,
-            geofencing, and alert notification via email and SMS. I integrated with Qualcomm and customer back-end
-            systems using REST and JMS. I implemented the UI, data store, geofence drawing and detection, and alert
-            delivery.
-        </p>
-        <p>
-            I developed an ASP.NET web application that integrates customer data with vehicle position streams to detect
-            business events. I Implemented the UI, customer integration interface, and business-rules state machine.
-        </p>
-
         <h2>Education</h2>
         <em>
             <a href="http://pdx.edu">Portland State University</a> - BS Information Systems and Quantitative Analysis -
@@ -186,4 +171,4 @@ export default () => (
             and communication.
         </p>
     </div>
-);
+)
